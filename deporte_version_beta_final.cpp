fbc73cdce5f6e44f799eb7566f80a7e006824f37
Terrosista <105392463+Terrosista@users.noreply.github.com>
2025-03-16 15:11:54 -0500

    Update deporte_version_beta_final.cpp

--- deporte_version_beta_final.cpp
+++ deporte_version_beta_final.cpp
@@ -2,348 +2,182 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
-#include <iostream>
 #include <fstream>
-/* Autores
-
-jose alberto rodriguez paname駉
-gilary daniela varga hurtado
- 
-
-*/
-
 
 using namespace std;
+
 struct Tiempo {
-	int time;
-	int cont;
-	int segtime;
-	int promedio;
-	
-	
+    int time;
+    int cont;
+    int segtime;
+    float promedio;
 };
 
-
-struct nodo{
-	
- 	char  nombre[25];
- 	int camisa;
- 	int valu;
- 	int da;
- 	Tiempo tiempo ;
- 	int contador;
- nodo *sig,*sig2;
-	
+struct nodo {
+    char nombre[25];
+    int camisa;
+    Tiempo tiempo;
+    int contador;
+    nodo *sig, *sig2;
 };
 
-
-
-nodo *cab,*aux,*cab1,*aux2;
+nodo *cab = NULL, *cab1 = NULL;
 
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
+void registrar() {
+    system("cls");
 
-int registrar(){
-system("cls");
+    nodo *nuevo = (nodo *) malloc(sizeof(nodo));
+    if (!nuevo) {
+        cout << "Error al asignar memoria." << endl;
+        return;
+    }
 
-	if(cab==NULL){
-	cab = (struct nodo *) malloc (sizeof(struct nodo));
-	cab->contador=0;
-	cab->tiempo.time=0;
-   cout<<"Ingrese numero de la Camisa : ";
-  
-	cin>>cab->camisa;
-	   cout<<"Ingrese el nombre  del participante : ";
-       cin>> cab->nombre;
-      cab1 =cab;
-      
-	cab1->sig2=NULL;
-	cab->sig =NULL;
+    nuevo->contador = 0;
+    nuevo->tiempo.time = 0;
+    nuevo->tiempo.promedio = 0;
+
+    cout << "Ingrese numero de la Camisa: ";
+    cin >> nuevo->camisa;
+    cout << "Ingrese el nombre del participante: ";
+    cin.ignore();
+    cin.getline(nuevo->nombre, 25);
+
+    nuevo->sig = NULL;
+    nuevo->sig2 = cab1;
+    cab1 = nuevo;
+
+    if (cab == NULL) {
+        cab = nuevo;
+    } else {
+        nodo *aux = cab;
+        while (aux->sig != NULL)
+            aux = aux->sig;
+        aux->sig = nuevo;
+    }
 
     system("cls");
-	 }else{
-	
-		
-		aux = (struct nodo *) malloc (sizeof(struct nodo));
-	    aux->contador=0;
-		aux->tiempo.time=0;	
-
-		cout<<"\n";
-   cout<<"Ingrese numero de la Camisa : ";
-
-	cin>>aux->camisa;
-	cout<<"Ingrese el nombre  del participante : ";
-       cin>> aux->nombre;
-	   //orden pila
-	    aux->sig2 = cab1;
-        cab1 = aux ;
-	   //fin
-	 aux->sig =NULL;
-	aux2 = cab;
-			 	
-	while(aux2->sig !=NULL)
-     	aux2 = aux2->sig;
-     	 
-	aux2->sig =aux;
-	
-	aux2 = aux = NULL;
-	
-	     aux =NULL;
-	     
-	  	free(aux);
-	  	free(aux2);
-	  	system("cls");
-	  		ofstream archivo("datos.txt");
-	
-	 
 }
 
+void tiempo() {
+    int camis;
+    bool encontrado = false;
 
+    cout << "Ingrese el numero de la camisa: ";
+    cin >> camis;
+
+    for (nodo *aux = cab; aux != NULL; aux = aux->sig) {
+        if (camis == aux->camisa) {
+            encontrado = true;
+
+            cout << "Ingrese el tiempo del corredor (segundos): ";
+            cin >> aux->tiempo.time;
+
+            aux->contador++;
+            aux->tiempo.promedio = ((aux->tiempo.promedio * (aux->contador - 1)) + aux->tiempo.time) / aux->contador;
+
+            cout << "Registro Exitoso" << endl;
+            return;
+        }
+    }
+
+    if (!encontrado) {
+        cout << "No se encontr贸 el corredor." << endl;
+    }
 }
 
-
-
-int tiempo(){
-	
-		aux = (struct nodo *) malloc (sizeof(struct nodo));
-		
-
-    aux->sig = NULL;
-    aux2 = cab;
-    
-   	bool  buscar= false ;
-	int camis;
-	 
-	int cont;
-   
-   cout<<"Ingrese el numero de la camisa"<<endl;
-   cin>>camis;
-   for (aux2 = cab; aux2 != NULL; aux2 = aux2->sig){
-   	
-   	  if(camis == aux2->camisa){
-   	
-   	 
-        buscar==true;
-		 
-   	  	cout<<"Ingrese el tiempo  del corrador"<<"|"<<"contador: "<<aux2->contador<<endl;
-   	  	cin>>aux2->tiempo.time;
-			 	
-   	 aux2->contador++;
-   	    aux2->tiempo.promedio = aux2->tiempo.time+aux2->tiempo.time/aux2->contador;
-   	  
-   	  aux2=NULL;
-   	  break; 
-   	  	cout<<"Registro Exitoso"<<endl;
-	 
-   	  
-	}
-		 
-		 
-		 
-		 if(buscar==false ){
-		 	
-		 	cout<<"No se encontro el corredor "<<endl;
-
-   	   	
-   	
-   }
-}
-
-
-	return 0;
-	
-}
-
-
-
-
-
-int mostrar(){
-	system("cls");
-	int i=1,c=1;
-	int a = 0;
-	 SetConsoleTextAttribute( hConsole,3);
-		cout<<" \t\t\tLista de tiempo de los participante "<<endl;
-		cout<<"\n";
-		cout<<"\n";
-		
-	
-	for(aux=cab; aux != NULL; aux=aux->sig){
-    
-	cout<<i++<<" Camisa de jugador: "<<aux->camisa<<"|"<<"Nombre: "<<aux->nombre <<"|"<<" Tiempo trascurido: "<<aux->tiempo.time<<endl;		  	              
-	  
-	    
-	
-		 
-		 }
-	
-}
-
-
-int mostraordenamiento(){
-	system("cls");
- int cont=1;
-   
- 
-  SetConsoleTextAttribute( hConsole,4);
-  
-
-	for( aux=cab1; aux != NULL; aux=aux->sig2){
-		 
-		 
-	cout<<cont++<<" : Camisa del jugador: "<<aux->camisa<<"|"<<" Nombre: "<<aux->nombre <<"|"<<"Tiempo"<<aux->tiempo.time<<endl;
-		
-
-	
-	}
-
- return 0;
- 
-}	
-	
-	
-	
-
-
-
-
-int mostra2(){
-	system("cls");
-	
-	int c=1;
-	int cambio;
-	int datos;
-	int i,j;
-	
-	 SetConsoleTextAttribute( hConsole,4);
-	 
-  	for( aux=cab1; aux != NULL; aux=aux->sig2){
-		 
-
-
-cout<<c++<<" :Numero de camisa del jugador: "<<aux->camisa<<""<<": El promedio: "<<aux->tiempo.promedio<<endl;
-	
-	     
-	  
-	  
-	
-
-}
-	
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-      // cout<<i++<<" promedio: "<<datos<<endl;
-       //cout<<"Numero de Participan "<<c++<<endl;
-}
-
-
-
-
-
-
-
-			
-         
-int eliminar(){
-	
- 	SetConsoleTextAttribute( hConsole,6);	
- 	
- if(cab != NULL){
- 
-	 aux = cab;
-	 cab =cab->sig;
-	 free(aux);
-	  cout<<" Fue eliminado "<<endl;
-	  
-	 system("pause");
+void mostrar() {
     system("cls");
- 	 
- 	
- 	
- }else 
- 
-		 	
-		 	cout<<"No se encontro el corredor "<<endl;
+    SetConsoleTextAttribute(hConsole, 3);
+    cout << "\t\t\tLista de tiempos de los participantes\n\n";
 
-
-    system("pause");
-    system("cls");
-   	   	
-  
-
-   return 0;
-
+    int i = 1;
+    for (nodo *aux = cab; aux != NULL; aux = aux->sig) {
+        cout << i++ << ". Camisa: " << aux->camisa << " | Nombre: " << aux->nombre
+             << " | Tiempo transcurrido: " << aux->tiempo.time << "s" << endl;
+    }
 }
 
+void mostraOrdenamiento() {
+    system("cls");
+    SetConsoleTextAttribute(hConsole, 4);
+    cout << "Lista de Ordenaci贸n Pila\n";
 
+    int cont = 1;
+    for (nodo *aux = cab1; aux != NULL; aux = aux->sig2) {
+        cout << cont++ << ". Camisa: " << aux->camisa << " | Nombre: " << aux->nombre
+             << " | Tiempo: " << aux->tiempo.time << "s" << endl;
+    }
+}
 
+void mostraPromedio() {
+    system("cls");
+    SetConsoleTextAttribute(hConsole, 4);
+    cout << "Lista de Promedio de tiempos\n";
 
-int main(){
-    int opc=0;
-    
-    
-    SetConsoleTextAttribute( hConsole,1);
-    
-    
-    do{  cout<<"\n";
-        cout<<"\tmenu"<<endl;
-         cout<<"\n";
-      cout<<"1.Regresar participante"<<endl;
-      cout<<"2.Colacar tiempo de llegada"<<endl;
-        cout<<"3.Ver lista de participante"<<endl;
-        cout<<"4.Promedio  "<<endl;
-        cout<<"5.Ordenamiento "<<endl;
-        cout<<"6.Eliminacionar "<<endl;
-        cout<<"7.Salir"<<endl;
-        cin>>opc;
-        switch(opc){
+    int c = 1;
+    for (nodo *aux = cab1; aux != NULL; aux = aux->sig2) {
+        cout << c++ << ". Camisa: " << aux->camisa << " | Promedio: " << aux->tiempo.promedio << "s" << endl;
+    }
+}
+
+void eliminar() {
+    SetConsoleTextAttribute(hConsole, 6);
+
+    if (cab != NULL) {
+        nodo *aux = cab;
+        cab = cab->sig;
+        free(aux);
+        cout << "El corredor ha sido eliminado." << endl;
+    } else {
+        cout << "No hay corredores registrados." << endl;
+    }
+}
+
+int main() {
+    int opc = 0;
+
+    do {
+        SetConsoleTextAttribute(hConsole, 1);
+        cout << "\n\tMENU\n";
+        cout << "1. Registrar participante\n";
+        cout << "2. Colocar tiempo de llegada\n";
+        cout << "3. Ver lista de participantes\n";
+        cout << "4. Mostrar promedios\n";
+        cout << "5. Mostrar ordenamiento\n";
+        cout << "6. Eliminar participante\n";
+        cout << "7. Salir\n";
+        cout << "Seleccione una opci贸n: ";
+        cin >> opc;
+
+        switch (opc) {
             case 1:
-            cout<<"Registro"<<endl; registrar();
-				break;
-				   case 2:
-			 cout<<"Regritro de Tiempo"<<endl;tiempo();
-			 
-			break;
-            case 3:
-            	
-			 cout<<"Mostrar lista de participante"<<endl;mostrar();
-			  
-			break;
-			case 4:
-				 cout<<"Lista de Promedio"<<endl;mostra2();
-				break;
-				
-				case 5:
-					
-				 cout<<"Lista de  Ordenacion Pila"<<endl;mostraordenamiento(); 
-				break;
-				
-				case 6: 
-				eliminar();
-				break;
-					
-            case 7:
-                cout<<"Salir";
+                registrar();
                 break;
-                default:
-                    cout<<"Opcion no valida....";
-                break;    
-            }
+            case 2:
+                tiempo();
+                break;
+            case 3:
+                mostrar();
+                break;
+            case 4:
+                mostraPromedio();
+                break;
+            case 5:
+                mostraOrdenamiento();
+                break;
+            case 6:
+                eliminar();
+                break;
+            case 7:
+                cout << "Saliendo..." << endl;
+                break;
+            default:
+                cout << "Opci贸n no v谩lida. Intente de nuevo." << endl;
+                break;
+        }
+    } while (opc != 7);
 
-        }while(opc!=7);
-		} 
-        
-        
-        
-        
-    
+    return 0;
+}
